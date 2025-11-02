4ba83a2facc3282090a4f5644fca84e4be0991ee
Deepa Kini <Deepa.Kini@Triconinfotech.com>
2025-11-02 17:47:32 +0530

    Fix vite base path for Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// ✅ Set correct base for Vercel
 export default defineConfig({
   plugins: [react()],
+  base: "./", // very important for Vercel
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+  },
 });
