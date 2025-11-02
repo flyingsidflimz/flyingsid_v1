86f12d7b5e3af546fcf7b2ca1ef29a254f846ba7
Deepa Kini <Deepa.Kini@Triconinfotech.com>
2025-11-02 16:49:56 +0530

    Fix vite permissions for Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
+#!/usr/bin/env node
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ✅ Works perfectly with Vercel and GitHub
 export default defineConfig({
   plugins: [react()],
   base: "./",
