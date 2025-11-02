b2dd6c14295832e98654c9c2e72f0bb9f2abe8e7
Deepa Kini <Deepa.Kini@Triconinfotech.com>
2025-11-02 19:18:18 +0530

    fix: clean build setup for vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // important for relative asset loading
+  base: './',
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist'
+  }
 });
