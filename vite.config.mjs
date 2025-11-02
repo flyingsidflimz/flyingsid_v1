bb3ac0f2a9b5410b9e6906624fb7bfb9becba2b9
Deepa Kini <Deepa.Kini@Triconinfotech.com>
2025-11-02 16:29:21 +0530

    Fix build config for Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.mjs
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
 });
